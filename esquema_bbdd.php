5930baa7fed0057bfecd731823fee443a01d0eed
fruize <fruize@outlook.es>
2018-12-21 13:33:46 +0100

    Añadido esquema base de datos

---
+++ esquema_bbdd.php
@@ -0,0 +1,143 @@
+user
+	id_user
+	email
+	password (hash)
+	type (??)
+
+type (pendiente de consulta ??)
+
+
+developer
+	id_developer
+	alias
+	first_name
+	last_name
+	address
+	postal_code
+	city
+	country
+	phone
+	link_github
+	link_linkedin
+	avatar
+	id_template
+
+training 
+	id_training
+	degree
+	institution
+	city
+	finish_date
+	id_developer (FK)
+
+experience
+	id_experience
+	position
+	company
+	finish_date
+	duration
+	id_developer (FK)
+
+additional
+	id_additional
+	description
+	id_developer (FK)
+
+recruiter
+	id_recruiter
+	name
+	link_github
+	link_linkedin
+	avatar
+
+company
+	id_company
+	company_name
+	alias
+	address
+	postal_code
+	city
+	country
+	contact_phone
+	link_github
+	link_linkedin
+	avatar
+
+academy
+	id_academy
+	academy_name
+	alias
+	address
+	postal_code
+	city
+	country
+	contact_phone
+	link_github
+	link_linkedin
+	avatar
+
+technology
+	id_technology
+	tech_name
+
+templates
+	id_template
+	template_name
+	thumbnail
+	designer (link a la pagina/github)
+	developer (link a la pagina/github)
+
+job_offer
+	id_job_offer
+	position
+	working_day
+	contract_type
+	salary
+	description
+	id_company (fk)	
+
+training_offer
+	id_training_offer
+	schedule
+	training_name
+	description
+	id_academy (fk)
+
+convenio-agreement (pendiente de implantación)
+
+
+search
+	id_search
+	search_name
+	date
+	id_user (fk)
+
+search_tech
+	id_search (fk)
+	id_technology (fk)
+			constraint->id_search + id_technology (pk)
+	preference
+
+tech_developer
+	id_developer (fk)
+	id_technology (fk)
+			constraint->id_developer + id_technology (pk)
+	level
+
+tech_company
+	id_company (fk)
+	id_technology (fk)
+			constraint->id_company + id_technology (pk)
+
+tech_job_offer
+	id_job_offer (fk)
+	id_technology (fk)
+			constraint->id_job_offer + id_technology (pk)	
+	preference
+
+tech_training_offer
+	id_training_offer (fk)
+	id_technology (fk)
+			constraint->id_training_offer + id_technology (pk)
+
+
