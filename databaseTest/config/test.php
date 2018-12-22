72e2ee5da62e8267a3f99120f969d31a6c93f67b
SergioVelmay <sergiovelmay@gmail.com>
2018-12-22 21:53:02 +0100

    Organización de archivos y carpetas para los TEST.

---
+++ databaseTest/config/test.php
@@ -0,0 +1,19 @@
+<?php
+
+// Número Total de Desarrolladores para probar las consultas complejas a la Base de Datos
+define('NUM_DEVELOPERS', 1);
+// Valores de Prueba: 1, 10, 100, 1.000, 10.000, 100.000 y 1.000.000
+
+// Número Máximo de Tecnologías que va a tener un desarrollador en su curriculum
+define('MAX_TECHNOLOGIES', 1);
+// Valores de Prueba: 1, 2, 5, 10, 15, 20 y 30
+
+// Número de Niveles de Destreza que puede elegir un Desarrollador para cada Tecnología
+define('NUM_LEVELS', 10)
+// Valores de Prueba: 3, 5 y 10 (de muy básico a gurú ninja)
+
+// Número Máximo de Tecnologías que se van a ordenar por preferencia en las búsquedas
+define('MAX_PREFERENCES', 1);
+// Valores de Prueba: 1, 2, 3, 4, 5, 10 y 20
+
+?>
---
+++ databaseTest/resources/technologies.php
@@ -0,0 +1,8 @@
+<?php
+
+  $tech = ["PHP", "JavaScript", "Java", "Python", "Swift", "Ruby", "C++", "C#", "C", "Visual Basic",
+  "Perl", "CSS", "HTML", "Angular", "Django", "Spring", "Yii", "Zend Framework", "Symfony", "ReactJS",
+  "Bootstrap", "Objetive-C", "Pascal", "Go", "Scala", "Rust", "Ruby on rails", "Laravel", "Node JS"];
+
+
+?>
