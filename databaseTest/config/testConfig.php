9c50940bf16a4d7cebbc0115df67b1763254a0f8
Francisco Ruiz <fruize@outlook.es>
2018-12-26 12:47:53 +0100

    Consultas de prueba fallidas

--- databaseTest/config/testConfig.php
+++ databaseTest/config/testConfig.php
@@ -1,11 +1,11 @@
 <?php
 
 // Número Total de Desarrolladores para probar las consultas complejas a la Base de Datos
-define('NUM_DEVELOPERS', 1);
+define('NUM_DEVELOPERS', 100);
 // Valores de Prueba: 1, 10, 100, 1.000, 10.000, 100.000 y 1.000.000
 
 // Número Máximo de Tecnologías que va a tener un desarrollador en su curriculum
-define('MAX_TECHNOLOGIES', 1);
+define('MAX_TECHNOLOGIES', 5);
 // Valores de Prueba: 1, 2, 5, 10, 15, 20 y 30
 
 // Número de Niveles de Destreza que puede elegir un Desarrollador para cada Tecnología
@@ -13,7 +13,7 @@ define('NUM_LEVELS', 10);
 // Valores de Prueba: 3, 5 y 10 (de muy básico a gurú ninja)
 
 // Número Máximo de Tecnologías que se van a ordenar por preferencia en las búsquedas
-define('MAX_PREFERENCES', 1);
+define('MAX_PREFERENCES', 3);
 // Valores de Prueba: 1, 2, 3, 4, 5, 10 y 20
 
 // Archivo donde se van a almacenar los registros generados aleatoriamente
