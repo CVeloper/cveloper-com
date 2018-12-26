4da5d8c3af07963e15cdcc395957c73401587f5c
Francisco Ruiz <fruize@outlook.es>
2018-12-26 10:44:08 +0100

    Public Creado

---
+++ databaseTest/resources/testInserts.sql
@@ -0,0 +1,47 @@
+
+/* ####################################################### RANDOM INSERTS */
+
+
+/* ####################################################### technology */
+
+INSERT INTO technology (tech_name) VALUES ('PHP');
+INSERT INTO technology (tech_name) VALUES ('JavaScript');
+INSERT INTO technology (tech_name) VALUES ('Java');
+INSERT INTO technology (tech_name) VALUES ('Python');
+INSERT INTO technology (tech_name) VALUES ('Swift');
+INSERT INTO technology (tech_name) VALUES ('Ruby');
+INSERT INTO technology (tech_name) VALUES ('C++');
+INSERT INTO technology (tech_name) VALUES ('C#');
+INSERT INTO technology (tech_name) VALUES ('C');
+INSERT INTO technology (tech_name) VALUES ('Visual Basic');
+INSERT INTO technology (tech_name) VALUES ('Perl');
+INSERT INTO technology (tech_name) VALUES ('CSS');
+INSERT INTO technology (tech_name) VALUES ('HTML');
+INSERT INTO technology (tech_name) VALUES ('Angular');
+INSERT INTO technology (tech_name) VALUES ('Django');
+INSERT INTO technology (tech_name) VALUES ('Spring');
+INSERT INTO technology (tech_name) VALUES ('Yii');
+INSERT INTO technology (tech_name) VALUES ('Zend Framework');
+INSERT INTO technology (tech_name) VALUES ('Symfony');
+INSERT INTO technology (tech_name) VALUES ('ReactJS');
+INSERT INTO technology (tech_name) VALUES ('Bootstrap');
+INSERT INTO technology (tech_name) VALUES ('Objetive-C');
+INSERT INTO technology (tech_name) VALUES ('Pascal');
+INSERT INTO technology (tech_name) VALUES ('Go');
+INSERT INTO technology (tech_name) VALUES ('Scala');
+INSERT INTO technology (tech_name) VALUES ('Rust');
+INSERT INTO technology (tech_name) VALUES ('Ruby on rails');
+INSERT INTO technology (tech_name) VALUES ('Laravel');
+INSERT INTO technology (tech_name) VALUES ('Node JS');
+
+/* ####################################################### search_tech */
+
+INSERT INTO search_tech (id_search, preference, id_technology) VALUES (1, 1, 25);
+
+/* ####################################################### developer */
+
+INSERT INTO developer (alias) VALUES ('etsollicitudinrutrum');
+
+/* ####################################################### developer_tech */
+
+INSERT INTO developer_tech (id_developer, id_technology, level) VALUES (1, 16, 9);
