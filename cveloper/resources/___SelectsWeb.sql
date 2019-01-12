7e88bae2205e020a43b21aa366af4701dde7a17a
SergioVelmay <sergiovelmay@gmail.com>
2019-01-12 20:18:36 +0100

    Añadido archivo para la lista de consultas.

---
+++ cveloper/resources/___SelectsWeb.sql
@@ -0,0 +1,27 @@
+SELECTS
+
+Registro de usuarios:
+INSERT mail y contraseña con ID_USUARIO NUMBER AUTOINCREMENT
+
+Validacion de usuarios:
+SELECT mail y contraseña JOIN de las tablas de usuarios.
+
+Datos personales, de empresa, de centro:
+INSERT formulario completo a cada tabla DESARROLLADOR, EMPRESA, CENTRO
+
+Dar de alta oferta empleo o formacion:
+INSERT datos del formulario en OFERTAS_EMPLEO u OFERTAS_FORMACION
+
+Listado de ofertas:
+SELECT todas las ofertas de EMPLEO o FORMACION con paginacion LIMIT y FROM
+
+Single de ofertas:
+SELECT datos con el ID_OFERTA
+
+Single de empresa y centro:
+SELECT datos con el ID
+
+Curriculum formateado:
+SELECT de todo con el ID_USUARIO
+
+(funciones get_nombre, get_foto, get_tecnologias, etc. para que todas las plantillas funcionen)
